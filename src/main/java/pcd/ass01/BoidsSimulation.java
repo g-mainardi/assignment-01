9ca5e508a0e6ce96df6284b33369951520a78458
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-03-31 14:38:18 +0200

    Changed initial width height ratio

--- src/main/java/pcd/ass01/BoidsSimulation.java
+++ src/main/java/pcd/ass01/BoidsSimulation.java
@@ -8,14 +8,14 @@ public class BoidsSimulation {
     final static double ALIGNMENT_WEIGHT = 1.0;
     final static double COHESION_WEIGHT = 1.0;
 
-    final static int ENVIRONMENT_WIDTH = 1000; 
+    final static int ENVIRONMENT_WIDTH = 1000;
 	final static int ENVIRONMENT_HEIGHT = 1000;
     static final double MAX_SPEED = 4.0;
     static final double PERCEPTION_RADIUS = 50.0;
     static final double AVOID_RADIUS = 20.0;
 
-	final static int SCREEN_WIDTH = 800; 
-	final static int SCREEN_HEIGHT = 800; 
+	final static int SCREEN_WIDTH = 1280;
+	final static int SCREEN_HEIGHT = 720;
 	
 
     public static void main(String[] args) {      
